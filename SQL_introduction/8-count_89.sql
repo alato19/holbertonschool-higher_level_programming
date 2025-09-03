535e046d045cea31f1dd207059238ab5a8f3b926
Aldion <aldion@gmail.com>
2025-09-03 13:26:11 +0200

    update 8 file with use db

--- SQL_introduction/8-count_89.sql
+++ SQL_introduction/8-count_89.sql
@@ -1,2 +1,4 @@
--- Write a script that displays the number of records with id = 89 in the table first_table of the database hbtn_0c_0 in your MySQL server.
-SELECT `id`,`name` FROM first_table WHERE id='89';
+-- Write a script that displays the number of records with id = 89
+USE hbtn_0c_0;
+
+SELECT * FROM first_table WHERE id='89';
